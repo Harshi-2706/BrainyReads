9b5c3ff44f2faddd259333a6292ae99bc739a3db
Harshitha <harshithabeere06@gmail.com>
2025-07-06 14:29:29 +0530

    Added live-server

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,24 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths instead of absolute
   server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   }
